695820702c8d3be0ed95699dd94fd31de30947fe
Pelle ten Cate <pelle@technoot.nl>
2020-09-29 15:03:52 +0200

    feat: Google Places address input that requires a valid address selection

---
+++ web/src/components/AddressForm/AddressForm.stories.js
@@ -0,0 +1,7 @@
+import AddressForm from './AddressForm'
+
+export const generated = () => {
+  return <AddressForm apiKey="AIzaSyDu9ThIQEQRiRMe00uL5Z9fRBlQwcqD76s" />
+}
+
+export default { title: 'Components/AddressForm' }
