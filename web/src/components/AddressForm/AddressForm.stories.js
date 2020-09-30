09b0244a341d76edf8a2463ca5cfa48325f13130
Pelle ten Cate <pelle@technoot.nl>
2020-09-30 17:18:51 +0200

    feat: switch to storybook

--- web/src/components/AddressForm/AddressForm.stories.js
+++
@@ -1,7 +0,0 @@
-import AddressForm from './AddressForm'
-
-export const generated = () => {
-  return <AddressForm apiKey="AIzaSyDu9ThIQEQRiRMe00uL5Z9fRBlQwcqD76s" />
-}
-
-export default { title: 'Components/AddressForm' }
