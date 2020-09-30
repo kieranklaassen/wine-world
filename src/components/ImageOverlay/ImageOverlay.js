306e65783f75116d1ff76e2754917dbc2a50ee9b
Kieran Klaassen <kieranklaassen@gmail.com>
2020-09-30 16:37:38 -0500

    feat: style close button on ImageOverlay

--- src/components/ImageOverlay/ImageOverlay.js
+++ src/components/ImageOverlay/ImageOverlay.js
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import clsx from "clsx";
 
-import { IconButton } from '@material-ui/core'
+import { IconButton } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 import "./../../index.css";
 
@@ -10,10 +11,16 @@ import ImageResizeContainer from "./../ImageResizeContainer/ImageResizeContainer
 const ImageOverlay = ({ image, close, withBackdrop }) => {
   return (
     <div className="absolute inset-0 flex justify-center w-full h-full overflow-y-auto" style={{ zIndex: 1200 }}>
-      {withBackdrop && <div onClick={close} className="fixed w-full h-full opacity-50 bg-vine-500 modal-overlay"></div>}
+      <div
+        onClick={close}
+        className={clsx([
+          "fixed w-full h-full bg-vine-500 modal-overlay",
+          { "opacity-50": withBackdrop, "opacity-0": !withBackdrop },
+        ])}
+      ></div>
       <div className="relative z-20 max-w-4xl mx-auto">
-        <div className="m-2 overflow-hidden rounded-lg shadow sm:shadow-lg sm:m-4 lg:shadow-2xl">
-          <div className="absolute top-0 right-0 z-30">
+        <div className="m-2 overflow-hidden rounded-lg shadow sm:shadow-lg sm:mt-8 lg:shadow-2xl">
+          <div className="absolute top-0 right-0 z-30 p-6 mt-6">
             <IconButton aria-label="close" onClick={close}>
               <CloseIcon />
             </IconButton>
@@ -23,18 +30,18 @@ const ImageOverlay = ({ image, close, withBackdrop }) => {
       </div>
     </div>
   );
-}
+};
 
 ImageResizeContainer.propTypes = {
   image: PropTypes.string.isRequired,
   close: PropTypes.func,
-  withBackdrop: PropTypes.bool
-}
+  withBackdrop: PropTypes.bool,
+};
 
 ImageOverlay.defaultProps = {
   image: null,
   close: undefined,
-  withBackdrop: false
-}
+  withBackdrop: false,
+};
 
-export default ImageOverlay
+export default ImageOverlay;
