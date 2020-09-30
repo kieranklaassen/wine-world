06187bac7edcc42598a356c375155168eb5ccfa1
Kieran Klaassen <kieranklaassen@gmail.com>
2020-09-30 12:30:40 -0500

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['src/*/**.js'],
+  theme: {
+    extend: {
+      colors: {
+        vine: {
+          50: "#F6F8F9",
+          100: "#ECF1F3",
+          200: "#D0DBE1",
+          300: "#B4C5CF",
+          400: "#7C9AAC",
+          500: "#446E88",
+          600: "#3D637A",
+          700: "#294252",
+          800: "#1F323D",
+          900: "#142129",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
