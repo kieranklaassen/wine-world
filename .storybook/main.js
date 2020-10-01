85dae36f417f63d125ed25154200f5e4f8e72e3c
Pelle ten Cate <pelle@technoot.nl>
2020-10-01 19:07:15 +0200

    fix: default config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-viewport']
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
 }
